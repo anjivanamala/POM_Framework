c25b0f4e06a49d613040f62e41dd2c721ed0d643
Avanamala <59122707+Avanamala@users.noreply.github.com>
2020-01-06 22:50:43 +0530

    Added Cucumber TestNg Runner Class

---
+++ src/test/java/runners/CucumberTestNG.java
@@ -0,0 +1,25 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberFeatureWrapper;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        features = "src/test/resources/com/agility/focis/jp/fcl",
+        glue={""},
+        tags = {"@TEST1"},
+        name = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        })
+public class CucumberTestNG extends AbstractTestNGCucumberTests {
+
+
+}
